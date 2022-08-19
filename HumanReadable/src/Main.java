827dcf979ae69514dca4dd024a0f32d4cce94bf4
Jamal Alabdullah <jamal.alabdullah@no.experis.com>
2022-08-19 11:04:34 +0000

    Deleted HumanReadable/.idea/.gitignore, HumanReadable/.idea/misc.xml, HumanReadable/.idea/modules.xml, HumanReadable/.idea/vcs.xml, HumanReadable/out/production/HumanReadable/HumanReadableTime.class, HumanReadable/out/production/HumanReadable/Main.class, HumanReadable/src/HumanReadableTime.java, HumanReadable/src/Main.java, HumanReadable/HumanReadable.iml, .idea/.gitignore, .idea/misc.xml, .idea/modules.xml, .idea/vcs.xml, out/production/rpgjamalheroes/.gitignore, out/production/rpgjamalheroes/RPG-Diagram.pdf

--- HumanReadable/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        HumanReadableTime humanReadableTime= new HumanReadableTime();
-        humanReadableTime.makeReadable(87788);
-    }
-}
